89f2f4f69ed65e131cac06f25e7facaa40d4ade4
niroigen <niroigen@gmail.com>
2017-10-21 20:26:37 -0400

    Adding dependecies for jsoup

--- app/src/main/java/com/example/niroigensuntharam/elec390application/MainActivity.java
+++ app/src/main/java/com/example/niroigensuntharam/elec390application/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.niroigensuntharam.elec390application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
