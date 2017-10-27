26948d8abe54c70e4619fe3220efdb37ec34061c
niroigen <niroigen@gmail.com>
2017-10-26 20:20:44 -0400

    MainActivity.java --Added InitializeRooms() when application is launched first, this method will be called and return all the available rooms --VerifyIfAvailable() basically verifies whether a certain room is available or not, then if the room is available it will be added to RoomsNowAvailable --RefreshRooms() Basically if the user wants to press the "REFRESH" button, this method will be called and RoomsNowAvailable will be modified ****** Please make sure to implement a button that will REFRESH
    Room.java
    --Added a room class that will store all the information (Capacity, Class Names, Room Number, and Time Slots)

--- app/src/main/java/com/example/niroigensuntharam/elec390application/MainActivity.java
+++ app/src/main/java/com/example/niroigensuntharam/elec390application/MainActivity.java
@@ -2,65 +2,91 @@ package com.example.niroigensuntharam.elec390application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    String dateString = new SimpleDateFormat("yyyyMMdd").format(new Date());
+    String timeString = new SimpleDateFormat("HHmm").format(new Date());
+
+    static String[][] AvailableRooms = {{"807","811","813","815","817","819","821","823","825","827","831"
+                                        ,"833","835","837","841","843","845","847","849","854","862"},
+                                        {"16","20","16","18","22","20","16","20","16","16","30","16"
+                                         ,"16","15","15","20","16","20","23","16","12"}};
+
+    static ArrayList<Room> Rooms = new ArrayList<>();
+    static ArrayList<Room> RoomsNowAvailable = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        parser("847");
+        InitializeRooms();
     }
 
-    public static void parser(String roomNumber)
+    private void InitializeRooms()
     {
+        Rooms.clear();
+        RoomsNowAvailable.clear();
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        Date date = new Date();
-        System.out.println(dateFormat.format(date));
+        for (int i = 0; i < AvailableRooms[0].length; i ++)
+        {
+            Room room = new Room(AvailableRooms[0][i],AvailableRooms[1][i]);
 
-        String da = dateFormat.format(date);
+            VerifyIfAvalaible(room);
 
-            Thread newThread = new Thread(){
-                public void run(){
-                    try {
-                        Document doc = Jsoup.connect("https://calendar.encs.concordia.ca/month.php?user=_NUC_LAB_H847").get();
-                        Elements elements = doc.select("a[class=layerentry]");
+            Rooms.add(room);
+        }
+    }
 
-                        String date = "20171002";
+    private void VerifyIfAvalaible(Room room)
+    {
+        boolean isAvailable = true;
 
-                        Element element = elements.select("a[href*=" + date + "]").first();
+        for (int i = 0; i < room.TimeList.size(); i++)
+        {
+            String[] time = room.TimeList.get(i).split("-");
 
-                        String eventid = element.attributes().get("id");
+            String[] startTime = time[0].split(":");
 
-                        Element event = doc.select("dl#eventinfo-" + eventid).first();
+            String[] endTime = time[1].split(":");
 
+            int TimeNow = Integer.parseInt(timeString);
+            int StartTime = Integer.parseInt(startTime[0].trim() + startTime[1].trim());
+            int EndTime = Integer.parseInt(endTime[0].trim() + endTime[1].trim());
 
-                        String hw="";
+            if (TimeNow >= StartTime && TimeNow < EndTime)
+            {
+                isAvailable = false;
+                break;
+            }
+        }
 
-                    }
-                    catch (Exception ex)
-                    {
-                        String Hello ="";
+        if (isAvailable)
+            RoomsNowAvailable.add(room);
+    }
 
-                    }
-                }
-            };
+    private void RefreshRooms()
+    {
+        String tempDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        String tempTime = new SimpleDateFormat("HH:mm").format(new Date());
 
-            newThread.start();
+        // If after the user refreshes, and there is a change in the date
+        // all the rooms will be initialized again
+        if (tempDate !=dateString)
+            InitializeRooms();
+
+        else if (timeString != tempTime)
+        {
+            RoomsNowAvailable.clear();
+
+            for (int i = 0; i < Rooms.size(); i++)
+                VerifyIfAvalaible(Rooms.get(i));
+        }
     }
 }
-
-
-
---
+++ app/src/main/java/com/example/niroigensuntharam/elec390application/Room.java
@@ -0,0 +1,81 @@
+package com.example.niroigensuntharam.elec390application;
+
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class Room {
+
+    ArrayList<String> TimeList = new ArrayList<>();
+    ArrayList<String> ClassList = new ArrayList<>();
+    String roomNumber;
+    String capacity;
+
+    Room (String room, String cap)
+    {
+        GetRoomInfoAsync getRoomInfoAsync = new GetRoomInfoAsync();
+
+        getRoomInfoAsync.execute("https://calendar.encs.concordia.ca/month.php?user=_NUC_LAB_H" + room);
+
+        roomNumber = room;
+        capacity = cap;
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+        final String dateString = dateFormat.format(date);
+
+        try {
+            Document document = getRoomInfoAsync.get();
+
+            Elements elements = document.select("a[class=layerentry]");
+
+            for (int i = 0 ; i < elements.select("a[href*=" + dateString + "]").size(); i++)
+            {
+                Element element = elements.select("a[href*=" + dateString + "]").get(i);
+
+                String eventid = element.attributes().get("id");
+
+                Element event = document.select("dl#eventinfo-" + eventid).first();
+
+                String temp = event.childNode(7).toString().split("\n")[1];
+
+                TimeList.add(temp);
+
+                temp = event.childNode(11).toString().split("\n")[1];
+
+                ClassList.add(temp);
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
+
+class GetRoomInfoAsync extends AsyncTask<String, Void, Document> {
+    @Override
+    protected Document doInBackground(String... params) {
+
+        try {
+            Document doc = Jsoup.connect(params[0]).get();
+
+            return doc;
+        }
+        catch (IOException ex)
+        {
+        }
+
+        return null;
+    }
+}
+
