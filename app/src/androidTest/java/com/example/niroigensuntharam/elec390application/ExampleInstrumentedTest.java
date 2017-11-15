7e027ec4712a17248692900dfed6aa76e58ad0a5
Niroigen Suntharam <niroigen@gmail.com>
2017-11-15 02:30:45 -0500

    Added Simple Test Cases use these to verify the application and add if there are any other test cases

---
+++ app/src/androidTest/java/com/example/niroigensuntharam/elec390application/ExampleInstrumentedTest.java
@@ -0,0 +1,52 @@
+package com.example.niroigensuntharam.elec390application;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void LongClickOnItem() {
+        onView(withId(R.id.simpleListView)).perform(longClick());
+    }
+
+    @Test
+    public void ClickOnItem(){
+        onView(withId(R.id.simpleListView)).perform(click());
+        onView(withId(R.id.currentRoomButton)).perform(click());
+    }
+
+    @Test
+    public void SwipeDown(){
+        onView(withId(R.id.simpleListView)).perform(swipeDown());
+    }
+}
---
+++ app/src/test/java/com/example/niroigensuntharam/elec390application/ExampleUnitTest.java
@@ -0,0 +1,39 @@
+package com.example.niroigensuntharam.elec390application;
+
+//import org.junit.Before;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import android.support.test.rule.ActivityTestRule;
+//import android.support.test.runner.AndroidJUnit4;
+//import android.test.suitebuilder.annotation.LargeTest;
+//
+//@RunWith(Android.class)
+//@LargeTest
+//public class ChangeTextBehaviorTest {
+//
+//    private String mStringToBetyped;
+//
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+//            MainActivity.class);
+//
+//    @Before
+//    public void initValidString() {
+//        // Specify a valid string.
+//        mStringToBetyped = "Espresso";
+//    }
+//
+//    @Test
+//    public void changeText_sameActivity() {
+//        // Type text and then press the button.
+//        onView(withId(R.id.editTextUserInput))
+//                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+//        onView(withId(R.id.changeTextBt)).perform(click());
+//
+//        // Check that the text was changed.
+//        onView(withId(R.id.textToBeChanged))
+//                .check(matches(withText(mStringToBetyped)));
+//    }
+//}
