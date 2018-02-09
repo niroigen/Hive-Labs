ebbc6e2ed2b73c60b5a993d4f6b5494773d77390
Niroingen Suntharam <niro@smarthalo.bike>
2018-02-09 11:49:20 -0500

    Cleaned up ContactsActivity, and removed custom_tab

--- app/src/main/java/com/itslegit/niroigensuntharam/hivelabs/ContactsActivity.java
+++ app/src/main/java/com/itslegit/niroigensuntharam/hivelabs/ContactsActivity.java
@@ -1,12 +1,15 @@
 package com.itslegit.niroigensuntharam.hivelabs;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.databinding.DataBindingUtil;
 import android.os.Build;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
@@ -17,6 +20,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.itslegit.niroigensuntharam.hivelabs.databinding.ActivityContactsBinding;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
@@ -26,55 +30,41 @@ import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
 
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    // Request code for SEND_SMS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_SEND_SMS = 99;
+    //list of contact names
+    final List<String> contacts = new ArrayList<>();
     //store the desired room number in order to SMS
     String roomNumber;
 
-    // The ListView with all contacts' names
-    private ListView lstNames;
-
-    //Button to send text
-    private Button getChoice;
-
-    // Request code for READ_CONTACTS. It can be any number > 0.
-    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
-
-    // Request code for SEND_SMS. It can be any number > 0.
-    private static final int PERMISSIONS_REQUEST_SEND_SMS = 99;
-
+    private ActivityContactsBinding binding;
     //array of hash to hold all contacts' name and phone number
-    private ArrayList<HashMap<String,String>> contactData=new ArrayList<HashMap<String,String>>();
-
-
-    //list of contact names
-    final List<String> contacts = new ArrayList<>();
-
+    private ArrayList<HashMap<String, String>> contactData = new ArrayList<>();
     //hold the default message
     private String message = "I am in room H-";
-
-    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contacts);
+
         roomNumber = getIntent().getExtras().getString("roomNumber");
 
         message = message + roomNumber;
 
-        // Find the list view
-        this.lstNames = (ListView) findViewById(R.id.lstNames);
-        getChoice = (Button) findViewById(R.id.getchoice);
-
-        searchView =(MaterialSearchView) findViewById(R.id.search_view);
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, contacts);
-        lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        lstNames.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, contacts);
+        binding.lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        binding.lstNames.setAdapter(adapter);
 
 
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+        binding.searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
             @Override
             public void onSearchViewShown() {
 
@@ -83,17 +73,14 @@ public class ContactsActivity extends AppCompatActivity {
             @Override
             public void onSearchViewClosed() {
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, contacts);
-                lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                lstNames.setAdapter(adapter);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, contacts);
+                binding.lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                binding.lstNames.setAdapter(adapter);
             }
         });
 
 
-
-
-
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+        binding.searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -101,30 +88,28 @@ public class ContactsActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                if (newText != null && !newText.isEmpty()){
+                if (newText != null && !newText.isEmpty()) {
 
-                    List<String> lstFound = new ArrayList<String>();
+                    List<String> lstFound = new ArrayList<>();
                     List<String> tempContacts = new ArrayList<>();
 
-                    for (int i=0; i < contacts.size(); i++){
+                    for (int i = 0; i < contacts.size(); i++) {
                         tempContacts.add(contacts.get(i).toUpperCase());
                     }
 
-                    for (int i=0; i < tempContacts.size(); i++){
-                        if (tempContacts.get(i).contains(newText.toUpperCase())){
+                    for (int i = 0; i < tempContacts.size(); i++) {
+                        if (tempContacts.get(i).contains(newText.toUpperCase())) {
                             lstFound.add(contacts.get(i));
                         }
                     }
 
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, lstFound);
-                    lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                    lstNames.setAdapter(adapter);
-                }
-
-                else{
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, contacts);
-                    lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                    lstNames.setAdapter(adapter);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, lstFound);
+                    binding.lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                    binding.lstNames.setAdapter(adapter);
+                } else {
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(ContactsActivity.this, android.R.layout.simple_list_item_multiple_choice, contacts);
+                    binding.lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                    binding.lstNames.setAdapter(adapter);
                 }
 
                 return true;
@@ -132,23 +117,21 @@ public class ContactsActivity extends AppCompatActivity {
         });
 
 
-
-
         // Read and show the contacts
         showContacts();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_contacts, menu);
         MenuItem item = menu.findItem(R.id.action_search);
-        searchView.setMenuItem(item);
+        binding.searchView.setMenuItem(item);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
@@ -161,59 +144,55 @@ public class ContactsActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        }
-
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.SEND_SMS}, PERMISSIONS_REQUEST_SEND_SMS);
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        }
-
-        else {
+        } else {
             // Android version is lesser than 6.0 or the permission is already granted.
 
             //final List<String> contacts = new ArrayList<>();
             getContactNames();
 
-            for (int i=0; i < contactData.size(); i++){
+            for (int i = 0; i < contactData.size(); i++) {
 
-                if (!contacts.contains(contactData.get(i).get("name"))){
+                if (!contacts.contains(contactData.get(i).get("name"))) {
                     contacts.add(contactData.get(i).get("name"));
                     Collections.sort(contacts); //sort contact names in alphabetic order
                 }
             }
-            
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, contacts);
-            lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-            lstNames.setAdapter(adapter);
 
-            getChoice.setOnClickListener(new Button.OnClickListener(){
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, contacts);
+            binding.lstNames.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+            binding.lstNames.setAdapter(adapter);
+
+            binding.getchoice.setOnClickListener(new Button.OnClickListener() {
 
                 @Override
-                public void onClick(View v){
+                public void onClick(View v) {
 
                     //variable to hold the contact's phone number
-                    String phoneNumber = "";
+                    String phoneNumber;
 
                     //List to hold contact names that were selected for SMS
                     List<String> selected = new ArrayList<>();
 
                     //temporary array of hash to hold the selected contacts' name and phone number for SMS
-                    ArrayList<HashMap<String,String>> selectedContacts = new ArrayList<HashMap<String,String>>();
+                    ArrayList<HashMap<String, String>> selectedContacts = new ArrayList<>();
 
-                    int cntChoice = lstNames.getCount();
-                    SparseBooleanArray sparseBooleanArray = lstNames.getCheckedItemPositions();
+                    int cntChoice = binding.lstNames.getCount();
+                    SparseBooleanArray sparseBooleanArray = binding.lstNames.getCheckedItemPositions();
 
-                    for(int i = 0; i < cntChoice; i++){
-                        if(sparseBooleanArray.get(i)) {
-                            selected.add(lstNames.getItemAtPosition(i).toString());
+                    for (int i = 0; i < cntChoice; i++) {
+                        if (sparseBooleanArray.get(i)) {
+                            selected.add(binding.lstNames.getItemAtPosition(i).toString());
                         }
                     }
 
                     //get the selected contact info and put in a separate array
-                    for (int i=0; i < selected.size(); i++){
-                        for (int j=0; j < contactData.size(); j++){
-                            if (selected.get(i).contentEquals(contactData.get(j).get("name"))){
+                    for (int i = 0; i < selected.size(); i++) {
+                        for (int j = 0; j < contactData.size(); j++) {
+                            if (selected.get(i).contentEquals(contactData.get(j).get("name"))) {
                                 selectedContacts.add(contactData.get(j));
                             }
                         }
@@ -221,7 +200,7 @@ public class ContactsActivity extends AppCompatActivity {
 
                     SmsManager sms = SmsManager.getDefault();
 
-                    for (int i=0; i < selectedContacts.size(); i++){
+                    for (int i = 0; i < selectedContacts.size(); i++) {
 
                         phoneNumber = selectedContacts.get(i).get("number");
                         phoneNumber = phoneNumber.replace("(", "");
@@ -231,9 +210,7 @@ public class ContactsActivity extends AppCompatActivity {
 
                         try {
                             sms.sendTextMessage(phoneNumber, null, message, null, null);
-                        }
-                        catch (Exception error) {
-                            String why = "";
+                        } catch (Exception ignored) {
                         }
 
                         Toast.makeText(ContactsActivity.this, "Sent to " + selectedContacts.get(i).get("name"), Toast.LENGTH_SHORT).show();
@@ -245,8 +222,8 @@ public class ContactsActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission is granted
@@ -254,9 +231,7 @@ public class ContactsActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Until you grant the permission, we cannot access your contacts", Toast.LENGTH_SHORT).show();
             }
-        }
-
-        else if (requestCode == PERMISSIONS_REQUEST_SEND_SMS) {
+        } else if (requestCode == PERMISSIONS_REQUEST_SEND_SMS) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission is granted
                 showContacts();
@@ -271,36 +246,33 @@ public class ContactsActivity extends AppCompatActivity {
      *
      * @return an array of hash of names.
      */
-    private ArrayList<HashMap<String,String>> getContactNames(){
+    private void getContactNames() {
 
         try {
-            Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            @SuppressLint("Recycle") Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            assert cursor != null;
             while (cursor.moveToNext()) {
                 try {
                     String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
                     String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    String hasPhone = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
                     if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
                         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                        assert phones != null;
                         while (phones.moveToNext()) {
                             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            HashMap<String, String> map = new HashMap<String, String>();
+                            HashMap<String, String> map = new HashMap<>();
                             map.put("name", name);
                             map.put("number", phoneNumber);
                             contactData.add(map);
                         }
                         phones.close();
                     }
-                }
-                catch (Exception e) {
+                } catch (Exception ignored) {
                 }
             }
-        }
-        catch (Exception error){
-            String c="";
+        } catch (Exception ignored) {
         }
 
         //return contactsData;
-        return contactData;
     }
 }
